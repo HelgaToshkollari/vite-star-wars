65d688c8f7d93d38e2ee7868a8bb7333730feac1
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-12-06 16:01:16 +0100

    adds components and scss

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,34 @@
-<script setup>
-
-</script>
-
 <template>
   <div>
-    
+    <TheHeader></TheHeader>
+    <div class="container">
+      <CharactersList></CharactersList>
+
+    </div>
+    <TheFooter></TheFooter>
   </div>
   
 </template>
 
-<style scoped>
+<script>
+import TheHeader from './components/TheHeader.vue';
+import TheFooter from './components/TheFooter.vue';
+import CharactersList from './components/CharactersList.vue';
+
+
+export default{
+  components:{ TheHeader , TheFooter, CharactersList },
+  data() {
+    return {
+      
+    }
+  },
+}
+
+
+</script>
+
+<style lang="scss">
+@use "./styles/general.scss";
 
 </style>
